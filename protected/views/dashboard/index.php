a60621a896eef9f9115ab1e535e1d38bb0221b44
mandy26 <mandykaye_19@hotmail.com>
2013-07-25 19:04:31 -0400

    more dashboard

---
+++ protected/views/dashboard/index.php
@@ -0,0 +1,25 @@
+<div class="container">
+	<div class="row">
+		<div class="span2">
+			<ul><?php foreach ($categories as $c)
+			{ ?>
+			<li><?php echo CHtml::link(CHtml::encode($c->name), 
+				array ('category/edit','id' => $c->id)) ?></li>
+			<?php 
+			} ?></ul>
+			
+			
+			
+		</div>
+		<div class="span10">
+			another something
+			
+			<ul><?php foreach ($accounts as $a)
+			{ ?>
+			<li><?php echo CHtml::link(CHtml::encode($a->name), 
+				array ('account/edit','id' => $a->id)) ?></li>
+			<?php 
+			} ?></ul>
+		</div>
+	</div>
+</div>
